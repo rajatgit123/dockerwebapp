7a9c321f48ba1064ae12c5da462d2ac1afe932f3
rajatgit123 <101028687+rajatgit123@users.noreply.github.com>
2022-06-26 16:38:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
       'sonar.projectDescription': 'This is Node JS Hello World Application',
       'sonar.projectName': 'nodejs-app',
       'sonar.projectKey': 'nodejs-app',
-      'sonar.sourceEncoding: 'UTF-8',
+      'sonar.sourceEncoding': 'UTF-8',
       'sonar.login': 'c97815b7556e0477e229730ad47a0aa9060ffe32',
       'sonar.language': 'js',
       'sonar.sources': '.',
