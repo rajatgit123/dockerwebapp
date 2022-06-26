7d1d25ecdf318726bd90f156eee273c4c2973e59
rajatgit123 <101028687+rajatgit123@users.noreply.github.com>
2022-06-27 00:34:24 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.14.132.224:9000',
+  serverUrl: 'http://3.145.11.22:9000',
     options : {
       'sonar.projectDescription': 'This is Node JS Hello World Application',
       'sonar.projectName': 'nodejs-app',
       'sonar.projectKey': 'nodejs-app',
       'sonar.sourceEncoding': 'UTF-8',
-      'sonar.login': 'c97815b7556e0477e229730ad47a0aa9060ffe32',
+      'sonar.login': 'b8ac66a236d39234e02ce5f9f4132c941feee81d',
       'sonar.language': 'js',
       'sonar.sources': '.',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
