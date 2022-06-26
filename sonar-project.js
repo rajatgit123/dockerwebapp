c908700421bdd292c51fba732571d97f1f54047f
rajatgit123 <101028687+rajatgit123@users.noreply.github.com>
2022-06-26 13:06:39 +0530

    Update and rename sonar-properties.js to sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.144.86.223:9000',
+  serverUrl: 'http://3.14.132.224:9000',
     options : {
       'sonar.projectDescription': 'This is Node JS Hello World Application',
       'sonar.projectName': 'nodejs-app',
       'sonar.projectKey': 'nodejs-app',
       'sonar.sourceEncoding: 'UTF-8',
-      'sonar.login': 'c23708428b7f543c971c13e2d05d3f4f2e0f773b',
+      'sonar.login': 'c97815b7556e0477e229730ad47a0aa9060ffe32',
       'sonar.language': 'js',
       'sonar.sources': '.',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
