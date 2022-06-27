7421cb4000a96e1f7254f4ab654cf213a452f741
rajatgit123 <101028687+rajatgit123@users.noreply.github.com>
2022-06-27 10:01:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.145.11.22:9000',
+  serverUrl: 'http://18.216.96.123:9000',
     options : {
       'sonar.projectDescription': 'This is Node JS Hello World Application',
       'sonar.projectName': 'nodejs-app',
